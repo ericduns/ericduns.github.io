df070d19c20d134a32f51853e5210e64f4c1c5d0
f00_B34rd <ericduns@gmail.com>
2018-04-03 14:45:19 -0600

    added new files
    website change with php

---
+++ contact-form-handler.php
@@ -0,0 +1,41 @@
+<?php
+  $phpSelf = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
+  $name = $email = $message = "";
+  $nameErr = $emailErr = $messageErr = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+      $nameErr = "Name is required";
+    } else {
+      $name = test_input($_POST["name"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+        $nameErr = "Only letters and white space allowed"; 
+      }
+    }
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format"; 
+      }
+    }
+    if (empty($_POST["message"])) {
+      $comment = "Please enter a message";
+    } else {
+      $comment = test_input($_POST["message"]);
+    }
+  }
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $message = wordwrap($message,70);
+  mail("ericduns@gmail.com","$email;$name;",$message);
+?>
